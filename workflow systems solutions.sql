7d19f262ba57886832b90cace6c56276be43ba7b
Intoxikot <netoxikot@gmail.com>
2022-04-07 11:15:48 +0500

    Решения workflow

--- workflow systems solutions.sql
+++ workflow systems solutions.sql
@@ -1,14 +1,17 @@
+-- 1. Вывести список имен сотрудников, получающих заработную плату большую, чем у непосредственного руководителя
 select worker.title
 from worker
 left join worker as chief
 on worker.chief_id = chief.worker_id
 where worker.salary > chief.salary;
 
+-- 2. Вывести список имен сотрудников, получающих максимальную зарплату в своем отделе 
 select worker.title
 from worker
 group by department_id
 having max(worker.salary);
 
+-- 3. Вывести список названий отделов, количество сотрудников в которых не превышает 3-х человек
 select department.title
 from department
 left join worker
@@ -16,12 +19,14 @@ on department.department_id = worker.department_id
 group by department.department_id
 having count(worker_id) <= 3;
 
+-- 4. Вывести список имен сотрудников, не имеющих назначенного руководителя, работающего в том же отделе
 select * 
 from worker
 left join worker as chief
 on worker.chief_id = chief.worker_id
 where chief.department_id <> worker.department_id;
 
+-- 5. Вывести список названий отделов с максимальной суммарной заплатой сотрудников
 select department.department_id, department.title, sum(salary) as salary
 from department
 left join worker
